6c4bb127ffe59480c40e4211e55c3ae529c8e816
a113221067 <a113221067@mail.shu.edu.tw>
2024-10-22 17:59:31 +0800

    113-10-22

---
+++ 113-10-22/Groupbox/Form1.cs
@@ -0,0 +1,10 @@
+namespace Groupbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- 113_1_C_Sharp_Tutorial-main/Tutorial 3-4/Test Average/Test Average/Form1.cs
+++ 113_1_C_Sharp_Tutorial-main/Tutorial 3-4/Test Average/Test Average/Form1.cs
@@ -17,6 +17,19 @@ namespace Test_Average
             InitializeComponent();
         }
 
-        
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            decimal Text1txetbox;
+            decimal Text2textbox;
+            decimal Text3textbox;
+            decimal averageLabel;
+
+            Text1txetbox = decimal.Parse(Text1txetbox.ToString());
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
