bd3b00a4abd9523935b1c524339dc8f1cf771b75
a113221067 <a113221067@mail.shu.edu.tw>
2024-11-05 17:50:08 +0800

    113-11-05

--- 113-10-22/Groupbox/obj/Debug/net8.0-windows/Groupbox.AssemblyInfo.cs
+++ 113-10-22/Groupbox/obj/Debug/net8.0-windows/Groupbox.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Groupbox")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+886702713fdd92b85ee9a004b3bc01374ed6f0fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4cdd0500eeee89baef23d5aa2a55ef6350e7fcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Groupbox")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Groupbox")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
