fc91f3ffde2c1b80280e19f8fba0185f89a1abb8
a113221067 <a113221067@mail.shu.edu.tw>
2024-12-10 17:56:50 +0800

    113-12-10

---
+++ 113-12-10/Tutorial 6-2/Cards/Cards/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cards
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void showAceSpades()
+        {
+
+        }
+        private void showCard(string cardName)
+        {
+            switch(cardName)
+            {
+                case "黑桃Ace"
+                    showAceSpades();
+                    case ""
+            }
+        }
+        private void showCardButton_Click(object sender, EventArgs e)
+        {
+           if (cardListBox.SelectedIndex != -1)
+            {
+                showCard(cardListBox.SelectedItem.ToString());
+            }
+           else
+            {
+                MessageBox.Show("選擇一張牌");
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+
+        private void cardListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
